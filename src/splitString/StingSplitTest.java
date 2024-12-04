d1255730303a85a5dc465ec5528fdc4a7bd185cb
wzseba <wzseba@gmail.com>
2024-12-03 22:55:19 -0300

    first commit string

---
+++ src/splitString/StingSplitTest.java
@@ -0,0 +1,14 @@
+package splitString;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StingSplitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/splitString/StringSplit.java
@@ -0,0 +1,15 @@
+package splitString;
+
+public class StringSplit {
+
+	public static String[] solution(String s) {
+		String[] solucion;
+
+		for (int i = 0; i < s.length(); i++) {
+
+		}
+
+		return solucion;
+	}
+
+}
