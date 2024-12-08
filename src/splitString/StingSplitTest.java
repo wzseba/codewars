d853269fe67fbc4d7c0de2be893c602e394fe911
wzseba <wzseba@gmail.com>
2024-12-08 20:58:24 -0300

    add: solucion

--- src/splitString/StingSplitTest.java
+++ src/splitString/StingSplitTest.java
@@ -1,14 +1,27 @@
 package splitString;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import org.junit.jupiter.api.Test;
 
 class StingSplitTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testEvenString() {
+		String s = "abcdef";
+		String s1 = "HelloWorld";
+		assertArrayEquals(new String[] { "ab", "cd", "ef" }, StringSplit.solution(s), "Should handle even string");
+		assertArrayEquals(new String[] { "He", "ll", "oW", "or", "ld" }, StringSplit.solution(s1),
+				"Should handle even string");
+	}
+
+	@Test
+	public void testOddString() {
+		String s = "abcde";
+		String s1 = "LovePizza";
+		assertArrayEquals(new String[] { "ab", "cd", "e_" }, StringSplit.solution(s), "Should handle odd string");
+		assertArrayEquals(new String[] { "Lo", "ve", "Pi", "zz", "a_" }, StringSplit.solution(s1),
+				"Should handle odd string");
 	}
 
 }
--- src/splitString/StringSplit.java
+++ src/splitString/StringSplit.java
@@ -3,13 +3,31 @@ package splitString;
 public class StringSplit {
 
 	public static String[] solution(String s) {
-		String[] solucion;
 
-		for (int i = 0; i < s.length(); i++) {
-
+		if (s.length() == 0) {
+			String[] letras = new String[0];
+			return letras;
 		}
 
-		return solucion;
+		String result = "";
+
+		if (s.length() % 2 != 0) {
+			s = s.concat("_");
+		}
+
+		int end = 0;
+		for (int i = 0; i < s.length(); i += 2) {
+			// solucion = s.split("(?<=\\G.{2})");
+			end += 2;
+
+			result += s.substring(i, end) + ",";
+		}
+
+		return result.split(",");
 	}
 
+	public static void main(String[] args) {
+		String[] res = StringSplit.solution("");
+		System.out.println((res.length));
+	}
 }
