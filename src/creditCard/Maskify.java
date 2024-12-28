00352e40c91f2674bf4f36003ffa91f25da81793
wzseba <wzseba@gmail.com>
2024-12-28 00:07:55 -0300

    add: solution

--- src/creditCard/Maskify.java
+++ src/creditCard/Maskify.java
@@ -3,6 +3,18 @@ package creditCard;
 public class Maskify {
 	public static String maskify(String str) {
 		/* https://www.codewars.com/kata/5412509bd436bd33920011bc/train/java */
-		throw new UnsupportedOperationException("Unimplemented");
+
+		if (str.length() <= 3) {
+			return str;
+		}
+
+		String codificado = "";
+		for (int i = 0; i < str.length() - 4; i++) {
+			codificado += "#";
+		}
+
+		return codificado + str.substring(str.length() - 4);
+
 	}
+
 }
