0aa254eb7d44d6c006f7bd5cd965d68a474c9344
wzseba <wzseba@gmail.com>
2025-01-08 23:09:06 -0300

    solution ok

--- src/findoutlier/FindOutlier.java
+++ src/findoutlier/FindOutlier.java
@@ -1,9 +1,24 @@
 package findoutlier;
 
 public class FindOutlier {
-
+	/* https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/java */
 	static int find(int[] integers) {
+		/* tambien se podria verificar los 3 primeros numeros de la matriz */
+		int par = 0;
+		int impar = 0;
+		int conti = 0;
+		int contp = 0;
 
-		return 0;
+		for (int i : integers) {
+			if (i % 2 == 0) {
+				contp++;
+				par = i;
+			} else {
+				conti++;
+				impar = i;
+			}
+		}
+
+		return contp > conti ? impar : par;
 	}
 }
