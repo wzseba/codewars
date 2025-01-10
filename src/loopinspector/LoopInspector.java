c7fd55be2898f9289592d5ab7cdb50eb23298467
wzseba <wzseba@gmail.com>
2025-01-09 23:55:16 -0300

    firts commit

---
+++ src/loopinspector/LoopInspector.java
@@ -0,0 +1,13 @@
+package loopinspector;
+
+import org.w3c.dom.Node;
+
+public class LoopInspector {
+
+	public int loopSize(Node node) {
+
+		return 0;
+
+	}
+
+}
---
+++ src/loopinspector/LoopTest.java
@@ -0,0 +1,14 @@
+package loopinspector;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class LoopTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
