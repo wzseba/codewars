64c8431165c07db9c91449e81dc878901af33286
sebastian <wzseba@gmail.com>
2025-02-24 23:40:58 -0300

    add: fase 1 detectar bucle

--- src/loopinspector/LoopInspector.java
+++ src/loopinspector/LoopInspector.java
@@ -1,10 +1,19 @@
 package loopinspector;
 
-import org.w3c.dom.Node;
-
 public class LoopInspector {
-
+	/* https://www.codewars.com/kata/52a89c2ea8ddc5547a000863/train/java */
 	public int loopSize(Node node) {
+
+		Node nodo1 = node;
+		Node nodo2 = node;
+
+		while (nodo2 != null && nodo2.getNext() != null) {
+			nodo1 = nodo1.getNext();
+			nodo2 = nodo2.getNext().getNext();
+
+			if (nodo1 == nodo2)
+				return 0;
+		}
 
 		return 0;
 
